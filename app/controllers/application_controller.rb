a465b32e6116b2903964808476102b8c7b302e7b
YJHoon <yjhoon0816@gmail.com>
2023-12-02 16:13:17 +0900

    panko serializer setting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  def serializer(object, serializer)
+    serializer.new.serialize_to_json(object)
+  end
+
+  def each_serializer(objects, serializer)
+    Panko::ArraySerializer.new(objects, each_serializer: serializer).to_json
+  end
 end
