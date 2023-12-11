113a55c483af1b0f2c5eeee7684fddaf26159671
YJHoon <yjhoon0816@gmail.com>
2023-12-11 19:40:53 +0900

    fix candidate gender callback action

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -10,7 +10,7 @@ class Candidate < ApplicationRecord
   before_save :check_gender_value, :if => :gender_changed?
 
   def check_gender_value
-    if ["남", "남성", "man", "male"].include?(self.gender)
+    if ["남", "남성", "man", "male", 0, "0"].include?(self.gender)
       self.gender = "male"
     else
       self.gender = "female"
