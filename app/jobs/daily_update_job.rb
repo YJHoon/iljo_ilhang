888dd513aa1e4497fbf15cfb4c16200ed097598b
YJHoon <yjhoon0816@gmail.com>
2024-01-14 20:30:12 +0900

    zeitwerk issue fix

--- app/jobs/daily_update_job.rb
+++ app/jobs/daily_update_job.rb
@@ -1,17 +1,7 @@
-class UpdateMembersJob < ApplicationJob
+class DailyUpdateJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    update_date = Crawling::GetLastUpdateService.new.open_api_member
-
-    return if update_date == Rails.cache.read(MEMBER_DATA_UPDATE_CACHE_KEY)
-
-    Rails.cache.write(MEMBER_DATA_UPDATE_CACHE_KEY, update_date)
-
-    # 캐싱된거랑 다를 경우 업데이트 진행
-    OpenApi::MemberDataService.new.update_members
-    crawling_update_member = Crawling::UpdateMemberDataService.new
-    crawling_update_member.update_member_image
-    crawling_update_member.update_member_seq_id
+    # TODO: 매일 update 해야하는 코드 작성. ex) 열려라 국회 상세페이지
   end
 end
