f153f3d47c33797f625c8e529b4b8cd49bd4ce54
YJHoon <yjhoon0816@gmail.com>
2024-01-14 20:43:46 +0900

    change member serializer

--- app/serializers/members_serializer.rb
+++ app/serializers/members_serializer.rb
@@ -1,5 +1,9 @@
 class MembersSerializer < Panko::Serializer
-  attributes :id, :name, :image, :party_name
+  attributes :id, :name, :image_url, :party_name, :show_info
+
+  def image_url
+    object.image_url
+  end
 
   def party_name
     object.political_party.present? ? object.political_party.name : "무소속"
