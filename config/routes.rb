bb4190d25eb7af5a75102614ad027877b10b14d1
YJHoon <yjhoon0816@gmail.com>
2024-01-31 16:00:35 +0900

    reject change

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  namespace :api do
-    resources :members, only: [:index, :show]
-  end
+  resources :members, only: [:index, :show]
   # Defines the root path route ("/")
   # root "posts#index"
 end
