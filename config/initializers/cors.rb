95e60f972342a4e46c467357a915f837ef1bae05
YJHoon <yjhoon0816@gmail.com>
2024-01-31 16:24:47 +0900

    cors change

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://ilzo-ilhang-front.vercel.app", "https://iljoilhang.com", "http://localhost:3000"
+    origins "https://ilzo-ilhang-front.vercel.app", "https://iljoilhang.com", "https://api.iljoilhang.com", "http://localhost:3000"
     # origins "*"
 
     resource "*",
